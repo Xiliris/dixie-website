b6ebff3f6daa30cae1eb3dffcb56d4021afc366a
Xiliris <trashyface22@gmail.com>
2024-05-09 18:01:42 +0200

    feature: Added animations and new colors

---
+++ src/animations/itemAnimation.js
@@ -0,0 +1,18 @@
+const itemAnimation = {
+  initial: {
+    y: 10,
+    opacity: 0,
+  },
+
+  animate: ({ index, time }) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      delay: time * index,
+    },
+  }),
+};
+
+export default itemAnimation;
