5d5121951efea4b8f8374e4caedd3d08d3896a52
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-07-11 16:06:31 +0200

    chore: Update npm dependencies for fontawesome to version 6.5.2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,10 +18,89 @@ function Navbar({ navType }) {
       }
     }
     getUser();
-  }, []);
+  }, [getUserData, setUser]);
+
+  function UserComponent({ id, avatar }) {
+    const [menuState, setMenuState] = useState("hidden");
+    const [arrowState, setArrowState] = useState("rotateX(0deg)");
+    function toggleProfileMenu() {
+      if (menuState === "hidden") {
+        setMenuState("active");
+        setArrowState("rotateX(180deg)");
+      } else {
+        setMenuState("hidden");
+        setArrowState("rotateX(0deg)");
+      }
+    }
+
+    return (
+      <div className="profile">
+        <div id="profile-menu" onClick={toggleProfileMenu}>
+          <span
+            className="material-symbols-outlined"
+            style={{ transform: arrowState }}
+          >
+            expand_more
+          </span>
+          <img
+            src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.webp?size=512`}
+            alt="User avatar"
+          />
+        </div>
+
+        <ul className={menuState}>
+          <Link to="/profile">
+            <li>
+              <span className="material-symbols-outlined">person</span>
+              Profile
+            </li>
+          </Link>
+          <Link to="/dashboard">
+            <li>
+              <span className="material-symbols-outlined">dashboard</span>
+              Dashboard
+            </li>
+          </Link>
+          <Link to={config.support_url}>
+            <li>
+              <span className="material-symbols-outlined">handshake</span>
+              Support
+            </li>
+          </Link>
+          <Link to="/#about">
+            <li>
+              <span className="material-symbols-outlined">description</span>
+              About
+            </li>
+          </Link>
+          <hr />
+          <Link to="/premium">
+            <li className="emphasis">
+              <span className="material-symbols-outlined">workspace_premium</span>
+              Pricing
+            </li>
+          </Link>
+          <Link to="/#features">
+            <li>
+              <span className="material-symbols-outlined">star</span>
+              Features
+            </li>
+          </Link>
+          <hr />
+          <Link to="/logout">
+            <li className="danger">
+              <span className="material-symbols-outlined">logout</span>
+              Logout
+            </li>
+          </Link>
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <nav className={navType}>
+
       <Link to="/">
         <img src={logo} alt="Dixie bot" className="logo" />
       </Link>
@@ -30,89 +109,13 @@ function Navbar({ navType }) {
         <UserComponent id={user.id} avatar={user.avatar} />
       ) : (
         <Link to={config.redirect_url}>
-          <p>Login</p>
+          <button id="login-btn">Log in</button>
         </Link>
       )}
     </nav>
   );
 }
 
-function UserComponent({ id, avatar }) {
-  const [menuState, setMenuState] = useState("hidden");
-  const [arrowState, setArrowState] = useState("rotateX(0deg)");
-  function toggleProfileMenu() {
-    if (menuState === "hidden") {
-      setMenuState("active");
-      setArrowState("rotateX(180deg)");
-    } else {
-      setMenuState("hidden");
-      setArrowState("rotateX(0deg)");
-    }
-  }
 
-  return (
-    <div className="profile">
-      <div id="profile-menu" onClick={toggleProfileMenu}>
-        <span
-          className="material-symbols-outlined"
-          style={{ transform: arrowState }}
-        >
-          expand_more
-        </span>
-        <img
-          src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.webp?size=512`}
-          alt="User avatar"
-        />
-      </div>
-
-      <ul className={menuState}>
-        <Link to="/profile">
-          <li>
-            <span className="material-symbols-outlined">person</span>
-            Profile
-          </li>
-        </Link>
-        <Link to="/dashboard">
-          <li>
-            <span className="material-symbols-outlined">dashboard</span>
-            Dashboard
-          </li>
-        </Link>
-        <Link to={config.support_url}>
-          <li>
-            <span className="material-symbols-outlined">handshake</span>
-            Support
-          </li>
-        </Link>
-        <Link to="/#about">
-          <li>
-            <span className="material-symbols-outlined">description</span>
-            About
-          </li>
-        </Link>
-        <hr />
-        <Link to="/premium">
-          <li className="emphasis">
-            <span className="material-symbols-outlined">workspace_premium</span>
-            Pricing
-          </li>
-        </Link>
-        <Link to="/#features">
-          <li>
-            <span className="material-symbols-outlined">star</span>
-            Features
-          </li>
-        </Link>
-        <hr />
-        <Link to="/logout">
-          <li className="danger">
-            <span className="material-symbols-outlined">logout</span>
-            Logout
-          </li>
-        </Link>
-      </ul>
-    </div>
-  );
-}
 
 export default Navbar;
